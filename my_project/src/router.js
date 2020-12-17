3f02476c3ae10c67b2ba1d299dde63802ae22a74
1134200395@qq.com <1134200395@qq.com>
2020-12-17 19:38:26 +0800

    100个CSS的ICON

--- my_project/src/router.js
+++ my_project/src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import Icon from './routes/Icon/icon'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/icon" exact component={Icon} />
       </Switch>
     </Router>
   );
